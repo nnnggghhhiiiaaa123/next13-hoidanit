f2cbd8fb44d5f9a1612fba77c6fa792b81eb13d6
nnnggghhhiiiaaa123 <oootui123@gmail.com>
2024-01-12 16:18:43 +0700

    video17

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div>Header Hoidanit</div>
+        {children}
+        <div>footer</div>
+        </body>
     </html>
   )
 }
